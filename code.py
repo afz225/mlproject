f683f6f7c5e1c2ff74f02a7eba65ea4b0dd0dcd4
ashabrawy <afz225@nyu.edu>
2021-05-17 09:37:42 +0400

    new code

--- code.py
+++ code.py
@@ -1,329 +1,111 @@
 from __future__ import unicode_literals, print_function, division
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-
-import numpy as np
-import pandas as pd
-
-import os
+from io import open
+import unicodedata
+import string
 import re
 import random
 
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
-# In[79]:
-
-
 SOS_token = 0
 EOS_token = 1
-MAX_LENGTH = 20
 
-#initialize Lang Class
+
 class Lang:
-   def __init__(self):
-       #initialize containers to hold the words and corresponding index
-       self.word2index = {}
-       self.word2count = {}
-       self.index2word = {0: "SOS", 1: "EOS"}
-       self.n_words = 2  # Count SOS and EOS
-
-#split a sentence into words and add it to the container
-   def addSentence(self, sentence):
-       for word in sentence.split(' '):
-           self.addWord(word)
-
-#If the word is not in the container, the word will be added to it, 
-#else, update the word counter
-   def addWord(self, word):
-       if word not in self.word2index:
-           self.word2index[word] = self.n_words
-           self.word2count[word] = 1
-           self.index2word[self.n_words] = word
-           self.n_words += 1
-       else:
-           self.word2count[word] += 1
-
-
-# In[80]:
-
-
-#Normalize every sentence
-def normalize_sentence(df, lang):
-   sentence = df[lang].str.lower()
-   sentence = sentence.str.replace('[^A-Za-z\s]+', '')
-   sentence = sentence.str.normalize('NFD')
-   sentence = sentence.str.encode('ascii', errors='ignore').str.decode('utf-8')
-   return sentence
-
-def read_sentence(df, lang1, lang2):
-   sentence1 = normalize_sentence(df, lang1)
-   sentence2 = normalize_sentence(df, lang2)
-   return sentence1, sentence2
-
-def read_file(loc, lang1, lang2):
-   df = pd.read_csv(loc, delimiter='|', header=None, names=[lang1, lang2])
-   return df
-
-def process_data(lang1,lang2):
-   df = read_file('training/%s-%s.txt' % (lang1, lang2), lang1, lang2)
-   print("Read %s sentence pairs" % len(df))
-   sentence1, sentence2 = read_sentence(df, lang1, lang2)
-
-   source = Lang()
-   target = Lang()
-   pairs = []
-   for i in range(len(df)):
-       if len(sentence1[i].split(' ')) < MAX_LENGTH and len(sentence2[i].split(' ')) < MAX_LENGTH:
-           full = [sentence1[i], sentence2[i]]
-           source.addSentence(sentence1[i])
-           target.addSentence(sentence2[i])
-           pairs.append(full)
-
-   return source, target, pairs
-
-
-# In[81]:
-
-
-def indexesFromSentence(lang, sentence):
-   return [lang.word2index[word] for word in sentence.split(' ')]
-
-def tensorFromSentence(lang, sentence):
-   indexes = indexesFromSentence(lang, sentence)
-   indexes.append(EOS_token)
-   return torch.tensor(indexes, dtype=torch.long, device=device).view(-1, 1)
-
-def tensorsFromPair(input_lang, output_lang, pair):
-   input_tensor = tensorFromSentence(input_lang, pair[0])
-   target_tensor = tensorFromSentence(output_lang, pair[1])
-   return (input_tensor, target_tensor)
-
-
-# In[82]:
-
-
-class Encoder(nn.Module):
-   def __init__(self, input_dim, hidden_dim, embbed_dim, num_layers):
-       super(Encoder, self).__init__()
-      
-       #set the encoder input dimesion , embbed dimesion, hidden dimesion, and number of layers 
-       self.input_dim = input_dim
-       self.embbed_dim = embbed_dim
-       self.hidden_dim = hidden_dim
-       self.num_layers = num_layers
-
-       #initialize the embedding layer with input and embbed dimention
-       self.embedding = nn.Embedding(input_dim, self.embbed_dim)
-       #intialize the GRU to take the input dimetion of embbed, and output dimention of hidden and
-       #set the number of gru layers
-       self.gru = nn.GRU(self.embbed_dim, self.hidden_dim, num_layers=self.num_layers)
-              
-   def forward(self, src):
-      
-       embedded = self.embedding(src).view(1,1,-1)
-       outputs, hidden = self.gru(embedded)
-       return outputs, hidden
-
-class Decoder(nn.Module):
-   def __init__(self, output_dim, hidden_dim, embbed_dim, num_layers):
-       super(Decoder, self).__init__()
-
-#set the encoder output dimension, embed dimension, hidden dimension, and number of layers 
-       self.embbed_dim = embbed_dim
-       self.hidden_dim = hidden_dim
-       self.output_dim = output_dim
-       self.num_layers = num_layers
-
-# initialize every layer with the appropriate dimension. For the decoder layer, it will consist of an embedding, GRU, a Linear layer and a Log softmax activation function.
-       self.embedding = nn.Embedding(output_dim, self.embbed_dim)
-       self.gru = nn.GRU(self.embbed_dim, self.hidden_dim, num_layers=self.num_layers)
-       self.out = nn.Linear(self.hidden_dim, output_dim)
-       self.softmax = nn.LogSoftmax(dim=1)
-      
-   def forward(self, input, hidden):
-
-# reshape the input to (1, batch_size)
-       input = input.view(1, -1)
-       embedded = F.relu(self.embedding(input))
-       output, hidden = self.gru(embedded, hidden)       
-       prediction = self.softmax(self.out(output[0]))
-      
-       return prediction, hidden
-
-class Seq2Seq(nn.Module):
-   def __init__(self, encoder, decoder, device, MAX_LENGTH=MAX_LENGTH):
-       super().__init__()
-      
-#initialize the encoder and decoder
-       self.encoder = encoder
-       self.decoder = decoder
-       self.device = device
-     
-   def forward(self, source, target, teacher_forcing_ratio=0.5):
-
-       input_length = source.size(0) #get the input length (number of words in sentence)
-       batch_size = target.shape[1] 
-       target_length = target.shape[0]
-       vocab_size = self.decoder.output_dim
-      
-#initialize a variable to hold the predicted outputs
-       outputs = torch.zeros(target_length, batch_size, vocab_size).to(self.device)
-
-#encode every word in a sentence
-       for i in range(input_length):
-           encoder_output, encoder_hidden = self.encoder(source[i])
-
-#use the encoder’s hidden layer as the decoder hidden
-       decoder_hidden = encoder_hidden.to(device)
-  
-#add a token before the first predicted word
-       decoder_input = torch.tensor([SOS_token], device=device)  # SOS
-
-#topk is used to get the top K value over a list
-#predict the output word from the current target word. If we enable the teaching force,  then the #next decoder input is the next word, else, use the decoder output highest value. 
-
-       for t in range(target_length):   
-           decoder_output, decoder_hidden = self.decoder(decoder_input, decoder_hidden)
-           outputs[t] = decoder_output
-           teacher_force = random.random() < teacher_forcing_ratio
-           topv, topi = decoder_output.topk(1)
-           input = (target[t] if teacher_force else topi)
-           if(teacher_force == False and input.item() == EOS_token):
-               break
-
-       return outputs
-
-
-# In[83]:
-
-
-teacher_forcing_ratio = 0.5
-
-def clacModel(model, input_tensor, target_tensor, model_optimizer, criterion):
-   model_optimizer.zero_grad()
-
-   input_length = input_tensor.size(0)
-   loss = 0
-   epoch_loss = 0
-   # print(input_tensor.shape)
-
-   output = model(input_tensor, target_tensor)
-
-   num_iter = output.size(0)
-   #print(num_iter)
-
-#calculate the loss from a predicted sentence with the expected result
-   for ot in range(num_iter):
-       loss += criterion(output[ot], target_tensor[ot])
-
-   loss.backward()
-   model_optimizer.step()
-   epoch_loss = loss.item() / num_iter
-
-   return epoch_loss
-
-def trainModel(model, source, target, pairs, num_iteration=20000):
-   model.train()
-
-   optimizer = optim.SGD(model.parameters(), lr=0.01)
-   criterion = nn.NLLLoss()
-   total_loss_iterations = 0
-
-   training_pairs = [tensorsFromPair(source, target, random.choice(pairs))
-                     for i in range(num_iteration)]
-  
-   for iter in range(1, num_iteration+1):
-       training_pair = training_pairs[iter - 1]
-       input_tensor = training_pair[0]
-       target_tensor = training_pair[1]
-
-       loss = clacModel(model, input_tensor, target_tensor, optimizer, criterion)
-
-       total_loss_iterations += loss
-
-       if iter % 5000 == 0:
-           avarage_loss= total_loss_iterations / 5000
-           total_loss_iterations = 0
-           print('%d %.4f' % (iter, avarage_loss))
-          
-   torch.save(model.state_dict(), 'mytraining.pt')
-   return model
-
-
-# In[ ]:
-
-
-def evaluate(model, input_lang, output_lang, sentences, max_length=MAX_LENGTH):
-   with torch.no_grad():
-       input_tensor = tensorFromSentence(input_lang, sentences[0])
-       output_tensor = tensorFromSentence(output_lang, sentences[1])
-  
-       decoded_words = []
-  
-       output = model(input_tensor, output_tensor)
-       # print(output_tensor)
-  
-       for ot in range(output.size(0)):
-           topv, topi = output[ot].topk(1)
-           # print(topi)
-
-           if topi[0].item() == EOS_token:
-               decoded_words.append('<EOS>')
-               break
-           else:
-               decoded_words.append(output_lang.index2word[topi[0].item()])
-   return decoded_words
-
-def evaluateRandomly(model, source, target, pairs, n=10):
-   for i in range(n):
-       pair = random.choice(pairs)
-       print('source '+ pair[0])
-       print('target '+pair[1])
-       output_words = evaluate(model, source, target, pair)
-       output_sentence = ' '.join(output_words)
-       print('predicted ' + output_sentence)
-
-
-# In[ ]:
-
-
-lang1 = 'demotic'
-lang2 = 'english'
-source, target, pairs = process_data(lang1, lang2)
-
-randomize = random.choice(pairs)
-print('random sentence {}'.format(randomize))
-
-#print number of words
-input_size = source.n_words
-output_size = target.n_words
-print('Input : {} Output : {}'.format(input_size, output_size))
-
-embed_size = 512
-hidden_size = 512
-num_layers = 3
-num_iteration = 100000
-
-#create encoder-decoder model
-encoder = Encoder(input_size, hidden_size, embed_size, num_layers)
-decoder = Decoder(output_size, hidden_size, embed_size, num_layers)
-
-model = Seq2Seq(encoder, decoder, device).to(device)
-
-#print model 
-print(encoder)
-print(decoder)
-
-#model = trainModel(model, source, target, pairs, num_iteration)
-evaluateRandomly(model, source, target, pairs)
-
-
-# In[ ]:
-
-
+    def __init__(self, name):
+        self.name = name
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {0: "SOS", 1: "EOS"}
+        self.n_words = 2  # Count SOS and EOS
+
+    def addSentence(self, sentence):
+        for word in sentence.split(' '):
+            self.addWord(word)
+
+    def addWord(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.n_words
+            self.word2count[word] = 1
+            self.index2word[self.n_words] = word
+            self.n_words += 1
+        else:
+            self.word2count[word] += 1
+
+# Turn a Unicode string to plain ASCII, thanks to
+# https://stackoverflow.com/a/518232/2809427
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+    )
+
+# Lowercase, trim, and remove non-letter characters
+
+
+def normalizeString(s):
+    s = unicodeToAscii(s.lower().strip())
+    s = re.sub(r"([.!?])", r" \1", s)
+    s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+    return s
+
+
+def readLangs(lang1, lang2, reverse=False):
+    print("Reading lines...")
+
+    # Read the file and split into lines
+    lines = open('data/%s-%s.txt' % (lang1, lang2), encoding='utf-8').\
+        read().strip().split('\n')
+
+    # Split every line into pairs and normalize
+    pairs = [[normalizeString(s) for s in l.split('|')] for l in lines]
+
+    # Reverse pairs, make Lang instances
+    if reverse:
+        pairs = [list(reversed(p)) for p in pairs]
+        input_lang = Lang(lang2)
+        output_lang = Lang(lang1)
+    else:
+        input_lang = Lang(lang1)
+        output_lang = Lang(lang2)
+
+    return input_lang, output_lang, pairs
+
+
+
+MAX_LENGTH = 10
+
+def filterPair(p):
+    return len(p[0].split(' ')) < MAX_LENGTH and \
+        len(p[1].split(' ')) < MAX_LENGTH and \
+        #p[1].startswith(eng_prefixes)
+
+
+def filterPairs(pairs):
+    return [pair for pair in pairs if filterPair(pair)]
+
+
+def prepareData(lang1, lang2, reverse=False):
+    input_lang, output_lang, pairs = readLangs(lang1, lang2, reverse)
+    print("Read %s sentence pairs" % len(pairs))
+    pairs = filterPairs(pairs)
+    print("Trimmed to %s sentence pairs" % len(pairs))
+    print("Counting words...")
+    for pair in pairs:
+        input_lang.addSentence(pair[0])
+        output_lang.addSentence(pair[1])
+    print("Counted words:")
+    print(input_lang.name, input_lang.n_words)
+    print(output_lang.name, output_lang.n_words)
+    return input_lang, output_lang, pairs
+
+
+input_lang, output_lang, pairs = prepareData('demotic', 'english')
+print(random.choice(pairs))
 
 
