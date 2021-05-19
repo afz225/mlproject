557eebd39f7cb36bacc1cdecc57dacc43b8f6f2c
ashabrawy <afz225@nyu.edu>
2021-05-19 14:22:56 +0400

    experiments

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 
+# coding=utf-8
 from code import prepareData, normalizeString, EncoderRNN, AttnDecoderRNN, evaluate, input_lang, output_lang, device
 
 import torch
@@ -25,38 +26,54 @@ hidden_size = 512
 encoder1 = EncoderRNN(input_lang.n_words, hidden_size)
 attn_decoder1 = AttnDecoderRNN(hidden_size, output_lang.n_words, dropout_p=0.05)
 
-encoder1.load_state_dict(torch.load('./myencoder.pt', map_location=torch.device('cpu')))
-attn_decoder1.load_state_dict(torch.load('./mydecoder.pt', map_location=torch.device('cpu')))
+encoder1.load_state_dict(torch.load('./experiments/standrew/myencoder.pt', map_location=torch.device('cpu')))
+attn_decoder1.load_state_dict(torch.load('./experiments/standrew/mydecoder.pt', map_location=torch.device('cpu')))
+
+encoder2.load_state_dict(torch.load('./experiments/standrew_and_beinlich/myencoder.pt', map_location=torch.device('cpu')))
+attn_decoder2.load_state_dict(torch.load('./experiments/standrew_and_beinlich/mydecoder.pt', map_location=torch.device('cpu')))
+
+encoder3.load_state_dict(torch.load('./experiments/translated_german_and_beinlich/myencoder.pt', map_location=torch.device('cpu')))
+attn_decoder3.load_state_dict(torch.load('./experiments/translated_german_and_beinlich/mydecoder.pt', map_location=torch.device('cpu')))
 
 # print(' '.join(evaluate(encoder1, attn_decoder1, normalizeString(input('input egyptian here: ')))[0]))
 
-import sacrebleu
-from sacremoses import MosesDetokenizer
-md = MosesDetokenizer(lang='en')
+
+
+from nltk.translate.bleu_score import sentence_bleu
 
 reference = []
-candidate = []
+candidate1 = []
+candidate2 = []
+candidate3 = []
 
-file = open('./training/egyptian-english.txt', 'r')
+reference.append(normalizeString('next to Anubis, Lord of the Holy Land, the chief of artworkers, the scribe and sculptor Irtysen. He says: I').split(' '))
 
-for i in range(1000):
-	line = file.readline().strip().split(';')
-	
-	if line[0] != '' and line[1] != '':
-		reference.append(md.detokenize(normalizeString(line[1]).strip().split(' ')))
-		temp = ' '.join(evaluate(encoder1, attn_decoder1, normalizeString(line[0]))[0])
-		temp = temp.strip()
-		temp = temp.split(' ')
-		temp.pop()
-		candidate.append(md.detokenize(temp))
+# candidate1.append(evaluate(encoder1, attn_decoder1, normalizeString('xr Inpw nb tA Dsr (i)m(y)-r(A) Hmw.t sS qs.ty Ir.ty=sn Dd(=f) iw rx'))[0].split(' '))
+# candidate2.append(evaluate(encoder2, attn_decoder2, normalizeString('xr Inpw nb tA Dsr (i)m(y)-r(A) Hmw.t sS qs.ty Ir.ty=sn Dd(=f) iw rx'))[0].split(' '))
+# candidate3.append(evaluate(encoder3, attn_decoder3, normalizeString('xr Inpw nb tA Dsr (i)m(y)-r(A) Hmw.t sS qs.ty Ir.ty=sn Dd(=f) iw rx'.replace('A', 'ꜣ').replace('j', 'i҆').replace('a', 'ꜥ').replace('H', 'ḥ').replace('x', 'ḫ').replace('X', 'ẖ').replace('S', 'š').replace('q', 'ḳ').replace('T', 'ṯ').replace('D','ḏ').replace('jj', 'y'))[0].split(' '))
 
 
 
-file.close()
+print('Cumulative 1-gram: %f' % sentence_bleu(reference, candidate, weights=(1, 0, 0, 0)))
+print('Cumulative 2-gram: %f' % sentence_bleu(reference, candidate, weights=(0.5, 0.5, 0, 0)))
+print('Cumulative 3-gram: %f' % sentence_bleu(reference, candidate, weights=(0.33, 0.33, 0.33, 0)))
+print('Cumulative 4-gram: %f' % sentence_bleu(reference, candidate, weights=(0.25, 0.25, 0.25, 0.25)))
 
-print(reference[501])
-print(candidate[501])
 
-print(len(reference))
-print(len(candidate))
-print('corpus_bleu: %f' % sacrebleu.corpus_bleu(reference, candidate))
+# print('Cumulative 1-gram: %f' % sentence_bleu(reference, candidate1, weights=(1, 0, 0, 0)))
+# print('Cumulative 2-gram: %f' % sentence_bleu(reference, candidate1, weights=(0.5, 0.5, 0, 0)))
+# print('Cumulative 3-gram: %f' % sentence_bleu(reference, candidate1, weights=(0.33, 0.33, 0.33, 0)))
+# print('Cumulative 4-gram: %f' % sentence_bleu(reference, candidate1, weights=(0.25, 0.25, 0.25, 0.25)))
+
+
+# print('Cumulative 1-gram: %f' % sentence_bleu(reference, candidate2, weights=(1, 0, 0, 0)))
+# print('Cumulative 2-gram: %f' % sentence_bleu(reference, candidate2, weights=(0.5, 0.5, 0, 0)))
+# print('Cumulative 3-gram: %f' % sentence_bleu(reference, candidate2, weights=(0.33, 0.33, 0.33, 0)))
+# print('Cumulative 4-gram: %f' % sentence_bleu(reference, candidate2, weights=(0.25, 0.25, 0.25, 0.25)))
+
+
+# print('Cumulative 1-gram: %f' % sentence_bleu(reference, candidate3, weights=(1, 0, 0, 0)))
+# print('Cumulative 2-gram: %f' % sentence_bleu(reference, candidate3, weights=(0.5, 0.5, 0, 0)))
+# print('Cumulative 3-gram: %f' % sentence_bleu(reference, candidate3, weights=(0.33, 0.33, 0.33, 0)))
+# print('Cumulative 4-gram: %f' % sentence_bleu(reference, candidate3, weights=(0.25, 0.25, 0.25, 0.25)))
+
